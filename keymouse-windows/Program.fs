b778d6ffb6fa62645fb7406d0d6e58dbd31734dc
a_ <m@marvelmathew.ca>
2015-10-06 15:59:51 -0500

    Fix enable/disable logic

--- keymouse-windows/Program.fs
+++ keymouse-windows/Program.fs
@@ -4,22 +4,25 @@ open System.Drawing
 
 let mutable enabled = true
 let mutable mousePos =
-    Cursor.Position <- new Point(0, 0);
-    new System.Drawing.Point(0, 0)
+    let p = new Point(0, 0);
+    Cursor.Position <- p 
+    p
 
 let onMouseMove (e: MouseEventArgs) =
     mousePos <- e.Location
 
 let moveMouse f =
-    let (x, y) = f mousePos.X mousePos.Y
-    let x' = if x < 0 then 0 else x
-    let y' = if y < 0 then 0 else y
+    if enabled
+    then
+        let (x, y) = f mousePos.X mousePos.Y
+        let x' = if x < 0 then 0 else x
+        let y' = if y < 0 then 0 else y
 
-    mousePos <- new Point(x', y')
-    Cursor.Position <- mousePos
+        mousePos <- new Point(x', y')
+        Cursor.Position <- mousePos
 
 let onKeyDown (e: KeyEventArgs) =
-    if enabled && (List.exists (fun key -> key = e.KeyCode) [Keys.W; Keys.A; Keys.S; Keys.D])
+    if enabled && List.exists (fun key -> key = e.KeyCode) [Keys.W; Keys.A; Keys.S; Keys.D]
     then
         match e.KeyCode with
         | Keys.W -> moveMouse (fun x y -> (x, y - 5))
@@ -28,10 +31,12 @@ let onKeyDown (e: KeyEventArgs) =
         | Keys.D -> moveMouse (fun x y -> (x + 5, y))
 
         e.SuppressKeyPress <- true
-    else if e.KeyCode = Keys.OemBackslash
+
+    else if e.KeyCode = Keys.OemPipe || e.KeyCode = Keys.OemBackslash
     then
        if enabled then e.SuppressKeyPress <- true
        enabled <- not enabled
+       printfn "Enabled: %A" enabled
 
 [<EntryPoint>]
 let main argv = 
