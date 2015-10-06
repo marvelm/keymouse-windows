193682db52e64d07986f09579e8a0b6d4e370915
a_ <m@marvelmathew.ca>
2015-10-06 14:31:40 -0500

    Reset mouse position on start

--- keymouse-windows/Program.fs
+++ keymouse-windows/Program.fs
@@ -1,4 +1,20 @@
-﻿[<EntryPoint>]
+﻿open Gma.System.MouseKeyHook
+open System.Windows.Forms
+open System.Drawing
+
+let mutable enabled = true
+let mutable mousePos =
+    Cursor.Position <- new Point(0, 0);
+    new System.Drawing.Point(0, 0)
+
+let onMouseMove (e: MouseEventArgs) =
+    mousePos <- e.Location
+    printfn "%A" mousePos
+
+[<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let hook = Hook.GlobalEvents();
+    hook.MouseMove.Add onMouseMove
+
+    Application.Run()
     0 // return an integer exit code
