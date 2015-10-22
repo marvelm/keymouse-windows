31e0e37b64c8d794ee94477282417b4f8a10bb26
a_ <m@marvelmathew.ca>
2015-10-22 01:13:49 -0500

    Replace System.IntPtr with nativeint

--- keymouse-windows/Program.fs
+++ keymouse-windows/Program.fs
@@ -5,28 +5,27 @@ open System.Runtime.InteropServices
 open WindowsInput
 
 module Native =
-    open System
 
     [<DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)>]
-    extern IntPtr GetDesktopWindow();
+    extern nativeint GetDesktopWindow();
 
     [<DllImport("user32.dll", CharSet = CharSet.Auto)>]
-    extern IntPtr FindWindow([<MarshalAs(UnmanagedType.LPTStr)>] string lpClassName,[<MarshalAs(UnmanagedType.LPTStr)>] string lpWindowName);
+    extern nativeint FindWindow([<MarshalAs(UnmanagedType.LPTStr)>] string lpClassName,[<MarshalAs(UnmanagedType.LPTStr)>] string lpWindowName);
 
     [<DllImport("user32.dll")>]
-    extern IntPtr SetParent(
-          IntPtr hWndChild,      // handle to window
-          IntPtr hWndNewParent   // new parent window
+    extern nativeint SetParent(
+          nativeint hWndChild,      // handle to window
+          nativeint hWndNewParent   // new parent window
     );
 
     [<DllImport("user32.dll", SetLastError = true)>]
-    extern int GetWindowLong(IntPtr hWnd, int nIndex);
+    extern int GetWindowLong(nativeint hWnd, int nIndex);
 
     [<DllImport("user32.dll", SetLastError = true)>]
-    extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
+    extern int SetWindowLong(nativeint hWnd, int nIndex, int dwNewLong);
 
     let GWL_EXSTYLE: int = -20;
-    let WS_EX_TRANSPARENT: int = 0x20; 
+    let WS_EX_TRANSPARENT: int = 0x20;
 
 let mutable enabled = true
 let mutable mousePos =
